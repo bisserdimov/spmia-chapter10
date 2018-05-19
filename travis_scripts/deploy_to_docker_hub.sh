1ad1ff89fe79754247e5a54d024675312dae0aba
bisserdimov <biss_dimov@hotmail.com>
2018-05-19 15:51:49 -0400

    tetet

--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,8 +1,8 @@
 echo "Pushing service docker images to docker hub ...."
 docker login -u bdimov -p Levski1914
-docker push johncarnell/tmx-authentication-service:$BUILD_NAME
-docker push johncarnell/tmx-licensing-service:$BUILD_NAME
-docker push johncarnell/tmx-organization-service:$BUILD_NAME
-docker push johncarnell/tmx-confsvr:$BUILD_NAME
-docker push johncarnell/tmx-eurekasvr:$BUILD_NAME
-docker push johncarnell/tmx-zuulsvr:$BUILD_NAME
+docker push bdimov/tmx-authentication-service:$BUILD_NAME
+docker push bdimov/tmx-licensing-service:$BUILD_NAME
+docker push bdimov/tmx-organization-service:$BUILD_NAME
+docker push bdimov/tmx-confsvr:$BUILD_NAME
+docker push bdimov/tmx-eurekasvr:$BUILD_NAME
+docker push bdimov/tmx-zuulsvr:$BUILD_NAME
