d5fdf1fd5663bd4e3be9a0b08178187b2e48a2b7
bisserdimov <biss_dimov@hotmail.com>
2018-05-19 17:48:49 -0400

    test 2

--- travis_scripts/build_services.sh
+++ travis_scripts/build_services.sh
@@ -1,4 +1,3 @@
 echo "Building with travis commit of $BUILD_NAME ..."
-mvn dependency:purge-local-repository -DactTransitively=false -DreResolve=false --fail-at-end
 mvn clean package docker:build
  
--- travis_scripts/deploy_to_amazon_ecs.sh
+++ travis_scripts/deploy_to_amazon_ecs.sh
@@ -1,5 +1,6 @@
 echo "Launching $BUILD_NAME IN AMAZON ECS"
 ecs-cli configure --region us-east-2 --cluster spmia-tmx-dev
+ecs-cli configure profile --access-key $AWS_ACCESS_KEY  --secret-key $AWS_SECRET_KEY 
 ecs-cli compose --file docker/common/docker-compose.yml up
 rm -rf ~/.ecs
  
