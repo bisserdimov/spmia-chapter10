5b125de44b5534ef99ff1b844dd14e835a6dfbf1
bisserdimov <biss_dimov@hotmail.com>
2018-05-19 18:07:09 -0400

    chaged

--- travis_scripts/deploy_to_amazon_ecs.sh
+++ travis_scripts/deploy_to_amazon_ecs.sh
@@ -1,6 +1,5 @@
 echo "Launching $BUILD_NAME IN AMAZON ECS"
-ecs-cli configure --region us-east-2 --cluster spmia-tmx-dev
-ecs-cli configure profile --access-key $AWS_ACCESS_KEY  --secret-key $AWS_SECRET_KEY 
-ecs-cli compose --file docker/common/docker-compose.yml up
+ecs-cli configure profile --access-key $AWS_ACCESS_KEY  --secret-key $AWS_SECRET_KEY
+ecs-cli compose --ecs-profile default --cluster spmia-tmx-dev  --region us-east-2 --file docker/common/docker-compose.yml up
 rm -rf ~/.ecs
  
