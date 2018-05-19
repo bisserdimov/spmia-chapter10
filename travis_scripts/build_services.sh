813fad813b5d9999490a4c78b82032fd3ac8a53a
bisserdimov <biss_dimov@hotmail.com>
2018-05-19 17:05:11 -0400

    test

--- travis_scripts/build_services.sh
+++ travis_scripts/build_services.sh
@@ -1,3 +1,4 @@
 echo "Building with travis commit of $BUILD_NAME ..."
+mvn dependency:purge-local-repository -DactTransitively=false -DreResolve=false --fail-at-end
 mvn clean package docker:build
  
